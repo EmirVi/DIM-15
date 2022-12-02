7ae280f2bd9de1654dcd4960536b243b314ee510
EmirVi <vildanov77@gmail.com>
2022-12-02 06:50:00 +0400

    add lab3 project with classes creating task

---
+++ AndroidStudioProjects/LabApplication/p0031firstproject/src/main/java/com/example/p0031firstproject/EmployeeTest.java
@@ -0,0 +1,25 @@
+package com.example.p0031firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.domain.Employee;
+
+public class EmployeeTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Employee emp = new Employee();
+        emp.setEmpId(101);
+        emp.setName("Jane Smith");
+        emp.setSalary(120_345.27);
+        emp.setSsn("012-34-5678");
+
+        System.out.println("Employee ID: "+emp.getEmpId());
+        System.out.println("Employee Name: "+emp.getName());
+        System.out.println("Employee Soc Sec # "+emp.getSsn());
+        System.out.println("Employee salary: "+emp.getSalary());
+    }
+}
