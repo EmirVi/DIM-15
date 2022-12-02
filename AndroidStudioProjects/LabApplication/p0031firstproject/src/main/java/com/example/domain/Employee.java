d4d51c2f6dfbbf6908bba200da8e42893e06db95
EmirVi <vildanov77@gmail.com>
2022-12-02 07:11:40 +0400

    complete class hierarchy task

--- AndroidStudioProjects/LabApplication/p0031firstproject/src/main/java/com/example/domain/Employee.java
+++ AndroidStudioProjects/LabApplication/p0031firstproject/src/main/java/com/example/domain/Employee.java
@@ -1,22 +1,16 @@
 package com.example.domain;
 
 public class Employee {
-    public int empId;
-    public String name;
-    public String ssn;
-    public double salary;
+    private int empId;
+    private String name;
+    private String ssn;
+    private double salary;
 
     public int getEmpId() { return empId; }
 
-    public void setEmpId(int empId) {
-        this.empId = empId;
-    }
+    public String getName() { return name; }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
@@ -24,18 +18,19 @@ public class Employee {
         return ssn;
     }
 
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
-
     public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
+    public Employee(int empId, String name, String ssn, double salary) {
+        this.empId = empId;
+        this.name = name;
+        this.ssn = ssn;
         this.salary = salary;
     }
 
-    public Employee() {
+    public void raiseSalary(double increase){
+        if (increase>0){ salary += increase;
+        }
     }
 }
--- AndroidStudioProjects/LabApplication/p0031firstproject/src/main/java/com/example/p0031firstproject/EmployeeTest.java
+++ AndroidStudioProjects/LabApplication/p0031firstproject/src/main/java/com/example/p0031firstproject/EmployeeTest.java
@@ -3,6 +3,10 @@ package com.example.p0031firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.domain.Employee;
+import com.example.domain.Admin;
+import com.example.domain.Director;
+import com.example.domain.Engineer;
+import com.example.domain.Manager;
 
 public class EmployeeTest extends AppCompatActivity {
 
@@ -11,15 +15,21 @@ public class EmployeeTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Employee emp = new Employee();
-        emp.setEmpId(101);
-        emp.setName("Jane Smith");
-        emp.setSalary(120_345.27);
-        emp.setSsn("012-34-5678");
+        Engineer eng = new Engineer(101, "Jane Smith", "012-34-5678", 120_345.27);
+        Manager mgr = new Manager(207, "Barbara Johnson", "054- 12- 2367", 109_501.36, "US Marketing");
+        Admin adm = new Admin(304, "Bill Munroe", "108-23- 2367", 75_002.34);
+        Director dir = new Director(12, "Susan Wheeler", "099- 45- 2340", 120_567.36, "Global Marketing", 1_000_000.00);
 
-        System.out.println("Employee ID: "+emp.getEmpId());
-        System.out.println("Employee Name: "+emp.getName());
-        System.out.println("Employee Soc Sec # "+emp.getSsn());
-        System.out.println("Employee salary: "+emp.getSalary());
+        printEmployee(eng);
+        printEmployee(mgr);
+        printEmployee(adm);
+        printEmployee(dir);
+    }
+
+    private static void printEmployee(Employee emp) {
+        System.out.println("Employee ID: " + emp.getEmpId());
+        System.out.println("Employee Name: " + emp.getName());
+        System.out.println("Employee Soc Sec # " + emp.getSsn());
+        System.out.println("Employee salary: " + emp.getSalary());
     }
 }
