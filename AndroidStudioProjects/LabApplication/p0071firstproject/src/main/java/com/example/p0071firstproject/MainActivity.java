2b17e53a1f6a76d168af34988ced38d890523bb7
EmirVi <vildanov77@gmail.com>
2023-03-24 00:16:38 +0400

    add lab7 with AsyncTask class

--- AndroidStudioProjects/LabApplication/p0071firstproject/src/main/java/com/example/p0071firstproject/MainActivity.java
+++ AndroidStudioProjects/LabApplication/p0071firstproject/src/main/java/com/example/p0071firstproject/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.p0071firstproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 
@@ -17,7 +16,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    private Handler handler;
     private TextView ipTextView;
     private ProgressBar progressBar;
 
@@ -28,45 +26,40 @@ public class MainActivity extends AppCompatActivity {
 
         ipTextView = findViewById(R.id.ipTextView);
         progressBar = findViewById(R.id.progressBar);
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                handleIPDeterminationMessage(msg);
-            }
-        };
     }
 
-    public void onDetermineIPAddressClick(View v) {
-        ipTextView.setText("");
-        progressBar.setVisibility(View.VISIBLE);
-        Thread determinationThread = new Thread() {
-            @Override
-            public void run() {
-                determineIPAddress();
+    private class IPDeterminationTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://api.ipify.org?format=json");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                String ip = "Your IP is " +
+                        (String) new JSONObject(reader.readLine()).get("ip");
+                reader.close();
+                return ip;
+            } catch (Exception e) {
+                return e.getMessage();
             }
-        };
-        determinationThread.start();
-    }
+        }
 
-    private void determineIPAddress() {
-        try {
-            URL url = new URL("https://api.ipify.org?format=json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.connect();
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String ip = "Your IP is " +
-                    (String) new JSONObject(reader.readLine()).get("ip");
-            reader.close();
-            handler.sendMessage(handler.obtainMessage(0, 0, 0, ip));
-        } catch (Exception e) {
-            handler.sendMessage(handler.obtainMessage(0, 0, 0, e.getMessage()));
+        @Override
+        protected void onPreExecute() {
+            ipTextView.setText("");
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String ip) {
+            progressBar.setVisibility(View.INVISIBLE);
+            ipTextView.setText(ip);
         }
     }
 
-    private void handleIPDeterminationMessage(Message msg) {
-        progressBar.setVisibility(View.INVISIBLE);
-        ipTextView.setText(msg.obj.toString());
+    public void onDetermineIPAddressClick(View v) {
+        new IPDeterminationTask().execute();
     }
 }
