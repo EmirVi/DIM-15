b732be3a0bc70af8b4aa9bc3ceb1812c7c6bed75
EmirVi <vildanov77@gmail.com>
2024-03-09 21:08:33 +0400

    Create Snake.py
    Создание файла Snake.py Импорт необходимых библиотек и инициализация Pygame. Определение цветов, которые будут использоваться в игре. Определение размеров окна игры и установка экрана. Определение размера блока змейки и скорости движения змейки.

---
+++ SoftwareEngineering/SnakeGame/snake.py
@@ -0,0 +1,43 @@
+import pygame
+import time
+import random
+
+# Инициализация Pygame
+pygame.init()
+
+# Определение цветов
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+# Установка размеров экрана
+dis_width = 800
+dis_height = 600
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Змейка на Pygame')
+
+# Установка параметров игрового процесса
+snake_block = 10
+snake_speed = 15
+
+# Определение шрифта и размера текста
+font_style = pygame.font.SysFont(None, 50)
+
+# Функция отрисовки змейки
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+# Функция отрисовки счета
+def Your_score(score):
+    value = font_style.render("Ваш счет: " + str(score), True, blue)
+    dis.blit(value, [0, 0])
+
+# Основная функция игры
+def gameLoop():
+    # Остальной код игры будет добавлен здесь
+    pass
+
+# Запуск игрового цикла
+gameLoop()
