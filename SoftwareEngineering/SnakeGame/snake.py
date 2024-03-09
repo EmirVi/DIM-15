560f246f97e87b5efd4df9c52ec18bb052877c6b
EmirVi <vildanov77@gmail.com>
2024-03-09 22:03:12 +0400

    Implementation basic game logic
    Добавление базового функционала перемещения змейки по экрану в зависимости от нажатых клавиш

--- SoftwareEngineering/SnakeGame/snake.py
+++ SoftwareEngineering/SnakeGame/snake.py
@@ -36,8 +36,44 @@ def Your_score(score):
 
 # Основная функция игры
 def gameLoop():
-    # Остальной код игры будет добавлен здесь
-    pass
+    game_over = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    while not game_over:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)
+        pygame.draw.rect(dis, green, [x1, y1, snake_block, snake_block])
+
+        pygame.display.update()
+
+        pygame.time.Clock().tick(snake_speed)
+
+    pygame.quit()
+    quit()
 
 # Запуск игрового цикла
 gameLoop()
